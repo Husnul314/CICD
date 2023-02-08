0ba494951698d23064b16e49e4b972562024f3e6
BANKSAMPOERNA\husnul.gunawan <husnul.gunawan@banksampoerna.com>
2023-02-08 14:41:11 +0700

    First Commit

---
+++ TryCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TryCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
